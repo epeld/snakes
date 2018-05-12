593c3ad14ceece57701838e1034d16dea800d199
Erik Peldan <erik.peldan@gmail.com>
2018-05-12 21:20:01 +0200

    Simplify main-module

--- main.py
+++ main.py
@@ -1,43 +1,31 @@
 import pygame, sys
-import random
 from pygame.locals import *
-import simulation
 
-WHITE=(255,255,255)
+import colors
+import geometry
+import time_utils
+import game as gm
+import graphics as gfx
+
+def process_events(game):
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            game.set_done()
+
 
 def main():
     pygame.init()
 
-    # Set up display
-    DISPLAY=pygame.display.set_mode((640,480),0,32)
-
-    DISPLAY.fill(WHITE)
-
-    # Set up event queue
-    events = []
-
-    simulation.setup()
-    start_ticks = pygame.time.get_ticks()
-    while True:
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
-
-        #
-        # This is a simulation-snippet
-        #
-        elapsed = pygame.time.get_ticks() - start_ticks
-        for event in simulation.get_events(elapsed):
-            (_ms,x,y,w,h,color) = event
-            pygame.draw.rect(DISPLAY,color,(x,y,w,h))
-        #
-        # End of simulation-snippet
-        #
-
+    game = gm.Game()
+    display = gfx.create_display()
+    while not game.get_done():
+        process_events(game)
         pygame.display.update()
 
         pygame.time.wait(500)
+
+    pygame.quit()
+    sys.exit()
 
 
 main()
