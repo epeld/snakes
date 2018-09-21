c9b8eace17b2a74dfa3e39c9197304d375b699e7
Erik Peldan <erik.peldan@gmail.com>
2018-09-21 19:53:51 +0200

    Support controlling the snake

--- snakes.py
+++ snakes.py
@@ -7,8 +7,7 @@ WHITE=(255,255,255)
 
 class Player(object):
     def __init__(self):
-        self.pos = (50, 100)
-        self.heading = 0
+        self.reset()
 
     def update(self, speed):
         dx = math.cos(self.heading) * speed
@@ -16,12 +15,33 @@ class Player(object):
         x = self.pos[0] + dx
         y = self.pos[1] + dy
         self.pos = x, y
+
+        if self.turning:
+            if self.turning == 'LEFT':
+                self.heading -= 0.05
+            elif self.turning == 'RIGHT':
+                self.heading += 0.05
         return self.pos
 
     def rect(self, size):
         x = self.pos[0] - size/2
         y = self.pos[1] - size/2
         return (x, y, size, size)
+
+    def turn_left(self):
+        self.turning = 'LEFT'
+
+    def turn_right(self):
+        self.turning = 'RIGHT'
+
+    def stop_turning(self):
+        self.turning = None
+
+
+    def reset(self):
+        self.pos = (50, 100)
+        self.heading = 0
+        self.turning = None
 
 
 def main():
@@ -40,14 +60,24 @@ def main():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    player.turn_left()
+                elif event.key == K_RIGHT:
+                    player.turn_right()
+                elif event.key == K_SPACE:
+                    DISPLAY.fill(WHITE)
+                    player.reset()
+            elif event.type == KEYUP:
+                player.stop_turning()
 
-        player.update(5)
-        player.heading = player.heading + 0.05
+        player.update(2)
+        #player.heading = player.heading + 0.05
         pygame.draw.rect(DISPLAY,(100,200,250), player.rect(10))
 
         pygame.display.update()
 
-        pygame.time.wait(50)
+        pygame.time.wait(10)
 
 
 main()
