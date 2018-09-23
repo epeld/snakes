5d5252f06c2998a5d1b56bfcce52391aebc9e110
Erik Peldan <erik.peldan@gmail.com>
2018-09-23 10:13:55 +0200

    Adapt game to support multiple player + overlay

--- snakes.py
+++ snakes.py
@@ -70,11 +70,17 @@ def main():
     pygame.init()
 
     player = Player()
-    
-    # Set up display
-    DISPLAY=pygame.display.set_mode((640,480),0,32)
+    players = [player]
 
-    DISPLAY.fill(WHITE)
+    recent_rects = FifoQueue()
+
+    # Set up display
+    DEPTH=32
+    DISPLAY=pygame.display.set_mode((640,480),0,DEPTH)
+    OVERLAY=pygame.surface.Surface(DISPLAY.get_size(),SRCALPHA,DEPTH)
+
+    BACKGROUND_COLOR=WHITE
+    DISPLAY.fill(BACKGROUND_COLOR)
 
     start_ticks = pygame.time.get_ticks()
     exit = False
@@ -89,16 +95,39 @@ def main():
                 elif event.key == K_RIGHT:
                     player.turn_right()
                 elif event.key == K_SPACE:
-                    DISPLAY.fill(WHITE)
+                    DISPLAY.fill(BACKGROUND_COLOR)
                     player.reset()
                 elif event.key == K_ESCAPE:
                     exit = True
             elif event.type == KEYUP:
                 player.stop_turning()
 
-        player.update(2)
-        #player.heading = player.heading + 0.05
-        pygame.draw.rect(DISPLAY,(100,200,250), player.rect(10))
+        player.update(speed=2)
+
+        # Generate the new rectangles for each player
+        rts = [p.rect(10) for p in players]
+        recent_rects.put(rts)
+
+        # player rects older than 10 generations get
+        # "persisted" to screen bitmap
+        if recent_rects.size() > 10:
+            oldest_player_rects = recent_rects.get()
+            for rt in oldest_player_rects:
+                pygame.draw.rect(DISPLAY,(100,200,250), rt)
+
+        # redraw overlay
+        OVERLAY.fill((255,255,255,0)) #transparent
+        for generation in recent_rects.get_items():
+            for rt in generation:
+                pygame.draw.rect(OVERLAY,(200,100,50), rt)
+        DISPLAY.blit(OVERLAY, (0,0))
+
+        # DISPLAY.lock()
+        # color = tuple(DISPLAY.get_at((int(rt[0]), int(rt[1]))))
+        # if tuple(DISPLAY.get_at((int(rt[0]), int(rt[1])))) != BACKGROUND_COLOR:
+        #     print("INTERSECT")
+        #     print(color)
+        # DISPLAY.unlock()
 
         pygame.display.update()
 
