a8a2c65fdcfd05fc8dd942fe2f7182a58616868e
Erik Peldan <erik.peldan@gmail.com>
2018-09-24 19:40:22 +0200

    Implement "color holes" in snakes

--- snakes.py
+++ snakes.py
@@ -6,6 +6,19 @@ from pygame.locals import *
 WHITE=pygame.color.Color(255,255,255)
 TRANSPARENT_COLOR=pygame.color.Color(255,255,255, 0)
 
+def darken(color):
+    return pygame.color.Color(
+        round(color.r * 0.5),
+        round(color.g * 0.5),
+        round(color.b * 0.5)
+    )
+
+def lighten(color):
+    return pygame.color.Color(
+        min(255, round(color.r * 1.5)),
+        min(255, round(color.g * 1.5)),
+        min(255, round(color.b * 1.5))
+    )
 
 def are_same_color(c1, c2):
     return c1.r == c2.r and c1.g == c2.g and c1.b == c2.b
@@ -19,11 +32,48 @@ def rect_corners(rect):
         (x + w, y + h)
     ]
 
+class VaryingWormStateInterval(object):
+    """A helper for keeping track of worm state. Toggles automatically between "on" and "off" with varying intervals"""
+    def __init__(self):
+        self.on = True
+        self.length = self.randomize_length(self.on)
+
+    def randomize_length(self, on):
+        if on:
+            return random.randint(50, 150)
+        else:
+            return random.randint(10, 15)
+
+    def update(self):
+        self.length -= 1
+        if self.length <= 0:
+            self.on = not self.on
+            self.length = self.randomize_length(self.on)
+
+    def is_on(self):
+        return self.on
+
+
 class Player(object):
     def __init__(self):
         self.reset()
+        self.color = pygame.color.Color(100,200,250)
+        self.state = VaryingWormStateInterval()
+
+    def get_color(self):
+        return self.color
+
+    def get_head_color(self):
+        return darken(self.color)
+
+    def get_hole_color(self):
+        return lighten(self.color)
+
+    def is_hole(self):
+        return not self.state.is_on()
 
     def update(self, speed):
+        self.state.update()
         dx = math.cos(self.heading) * speed
         dy = math.sin(self.heading) * speed
         x = self.pos[0] + dx
@@ -137,13 +187,18 @@ def main():
         if recent_rects.size() > 10:
             oldest_player_rects = recent_rects.get()
             for rt in oldest_player_rects:
-                pygame.draw.rect(COMMITTED_RECTS,(100,200,250), rt)
+                if player.is_hole():
+                    c = player.get_hole_color()
+                else:
+                    c = player.get_color()
+                pygame.draw.rect(COMMITTED_RECTS,c, rt)
 
         # redraw overlay
         TEMPORARY_RECTS.fill(TRANSPARENT_COLOR)
         for generation in recent_rects.get_items():
             for rt in generation:
-                pygame.draw.rect(TEMPORARY_RECTS,(200,100,50), rt)
+                c = player.get_head_color()
+                pygame.draw.rect(TEMPORARY_RECTS,c, rt)
 
         DISPLAY.blit(COMMITTED_RECTS, (0,0))
         DISPLAY.blit(TEMPORARY_RECTS, (0,0))
